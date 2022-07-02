43d2b1c7deb1bf06f877ae09c09a53968d349f0a
Safiraslw <90440818+Safiraslw@users.noreply.github.com>
2022-07-02 14:52:01 +0700

    commit

---
+++ SISFOM/database/seeders/KamarSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class KamarSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i=1; $i<=20; $i++){
+            DB::table('kamar')->insert([
+                'nomor' => rand(1,3),
+                'ruang' => 'Kenangas',
+                'status' => 'kosong',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+                ]);
+        }
+    }
+}
